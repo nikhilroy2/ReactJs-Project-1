6f72eb12388fc1862a8c7f14537be059182af234
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2020-02-21 10:09:06 +0600

    Updates

---
+++ precache-manifest.90a55254f52e5748012788b72c30aed5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a35f8c0325ddadcd11faa1d4591031",
+    "url": "./index.html"
+  },
+  {
+    "revision": "710d669887e0058bd529",
+    "url": "./static/css/main.f471e738.chunk.css"
+  },
+  {
+    "revision": "3a17e352ced7f0a65c12",
+    "url": "./static/js/2.ae2d6c3f.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.ae2d6c3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "710d669887e0058bd529",
+    "url": "./static/js/main.71f589a3.chunk.js"
+  },
+  {
+    "revision": "1d1feb9ac959fa64e717",
+    "url": "./static/js/runtime-main.33d7aa7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3a34eef23b9a086c3715df88b73a55e6",
+    "url": "./static/media/post.3a34eef2.png"
+  }
+]);
